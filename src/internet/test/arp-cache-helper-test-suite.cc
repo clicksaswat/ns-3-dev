32e71e8a34e778006759b3decb3d496933067f84
Saswat K. Mishra <clicksaswat@gmail.com>
2015-08-12 23:00:47 +0530

    extened the test file the test now uses ping application and pcap tracing to determine the state of arp supression

--- src/internet/test/arp-cache-helper-test-suite.cc
+++ src/internet/test/arp-cache-helper-test-suite.cc
@@ -12,11 +12,19 @@
 #include "ns3/csma-helper.h"
 #include "ns3/internet-stack-helper.h"
 #include "ns3/neighbor-table-helper.h"
+#include "ns3/v4ping-helper.h"
+#include "ns3/v4ping.h"
 
 using namespace ns3;
 
+NS_LOG_COMPONENT_DEFINE ("ArpCacheHelperTestSuite");
+
 int main (int argc, char *argv[])
 {
+  LogComponentEnable ("NeighborTableHelper", LOG_LEVEL_ALL);
+  LogComponentEnable ("ArpCache", LOG_LEVEL_ALL);
+  LogComponentEnable ("V4Ping", LOG_LEVEL_ALL);
+
   NodeContainer nodes;
   nodes.Create (10);
   Ptr<Node> host = nodes.Get (0);
@@ -25,6 +33,7 @@ int main (int argc, char *argv[])
   csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
   csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (5000)));
   NetDeviceContainer devices = csma.Install (nodes);
+  csma.EnablePcapAll ("arp-cache-test", BooleanValue (true));
   //Get device associated with host node
   Ptr<NetDevice> hostDevice = devices.Get (0);
 
@@ -42,10 +51,27 @@ int main (int argc, char *argv[])
   Ptr<Ipv4Interface> hostInterface = host->GetObject<Ipv4L3Protocol> ()->GetInterface (index);
 
   ArpCacheHelper arp;
-  Ptr<OutputStreamWrapper> output = Create<OutputStreamWrapper>(&std::cout);
+  Ptr<OutputStreamWrapper> output = Create<OutputStreamWrapper> (&std::cout);
   arp.PrintEntries (hostInterface, output);
+
   arp.PopulateArpCache (hostInterface);
   arp.PrintEntries (hostInterface, output);
+
+  ArpCache::Entry *entry = arp.GetEntry (hostInterface, "10.1.1.9");
+  arp.RemoveEntry (hostInterface, entry);
+  arp.PrintEntries (hostInterface, output);
+
+  V4PingHelper ping ("10.1.1.9");
+  ping.SetAttribute ("Interval", TimeValue (Seconds (5.0)));
+  ApplicationContainer pingApp = ping.Install (host);
+  pingApp.Start (Seconds (2.0));
+  pingApp.Stop (Seconds (20.0));
+
+
+  Ptr<NetDevice> remoteDevice = devices.Get (9);
+  ArpCache::Entry *newEntry = arp.MakeEntry ("10.1.1.9", remoteDevice->GetAddress ());
+  Simulator::Schedule (Seconds (5.0), &ArpCacheHelper::AddEntry, arp, hostInterface, newEntry);
+
 
   Simulator::Run ();
   Simulator::Destroy ();
